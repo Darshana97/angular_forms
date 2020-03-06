f285e0a5fd8e36b0f85bc12d87c4487b9d42a692
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-03-06 22:35:59 +0530

    form-validation

---
+++ tracking-state-and-validity/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
+import { error } from '@angular/compiler/src/util';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'tracking-state-and-validity';
+  public topics = ['Angular','React','Vue'];
+  topicHasError = true;
+
+  userModel = new User('Darshana','darshana@gmail.com',1234567890, 'default','morning',true);
+
+  constructor(private _enrollmentService: EnrollmentService){}
+
+  validateTopic(value){
+    if(value === 'default'){
+      this.topicHasError = true;
+    }else{
+      this.topicHasError = false;
+    }
+
+  }
+
+  onSubmit(){
+    this._enrollmentService.enroll(this.userModel).subscribe(data => console.log('Success!', data),error => console.error('Error!', error))
+  }
+
+}
