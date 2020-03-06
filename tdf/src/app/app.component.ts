be6664ee37ec71a606e88a1cca48878ddadfc1e0
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-03-06 12:38:07 +0530

    update

--- tdf/src/app/app.component.ts
+++ tdf/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'tdf';
   public topics = ['Angular','React','Vue'];
+
+  userModel = new User('Darshana','darshana@gmail.com',123456789, '','morning',true);
 }
